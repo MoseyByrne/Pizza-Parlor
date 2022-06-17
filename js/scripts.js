ca181df7e89239605e0ad4b0905fc9d0285eb8e6
MoseyByrne <mosey.b.byrne@gmail.com>
2022-06-17 11:16:29 -0700

    WIP html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+// Business Logic
+
+function Pizza(topping, size) {
+  this.topping = topping;
+  this.size = size;
+  this.price;
+}
 
 
-
+Pizza.prototype.price = function() {
+  this.price = 10;
+  if (this.size === "Large") {
+    this.price += 5;
+  } 
+  if (this.size === "Small") {
+    this.price += 0;
+  }
+  if (this.topping === "Pepperoni") {
+    this.price += 2;
+  }
+  console.log(this.price);
+}
